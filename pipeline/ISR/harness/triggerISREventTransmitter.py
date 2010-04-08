1302c1ebcdd7c2a9d7df2121dbd23a56898b6248
rallsman <rallsman@git.lsstcorp.org>
2010-04-08 23:08:04 +0000

    Updating policies for pipeline testing; adding sdqa stages.

--- pipeline/ISR/harness/triggerISREventTransmitter.py
+++ pipeline/ISR/harness/triggerISREventTransmitter.py
@@ -3,6 +3,10 @@
 import threading
 import lsst.daf.base as dafBase
 from lsst.daf.base import *
+import lsst.ip.isr as ipIsr
+import lsst.afw.cameraGeom.utils as cameraGeomUtils
+import lsst.pex.policy as pexPolicy
+import math
 import os
 import eups
 
@@ -14,15 +18,29 @@ if __name__ == "__main__":
     print "starting...\n"
 
     eventBrokerHost = "lsst8.ncsa.uiuc.edu"
-
     externalEventTransmitter = events.EventTransmitter(eventBrokerHost, "triggerISREvent")
 
+    obsDir = eups.productDir('obs_lsstSim') 
+    print 'obs_lsstSim:',obsDir
+    policyFile = pexPolicy.DefaultPolicyFile("afw",
+                "CameraGeomDictionary.paf", "policy")
+    defPolicy = pexPolicy.Policy.createPolicy(policyFile,
+                policyFile.getRepositoryPath(), True)
+    geomPolicy =\
+        pexPolicy.Policy.createPolicy(os.path.join(obsDir, "description", "Full_STA_geom.paf"), True)
+    geomPolicy.mergeDefaults(defPolicy.getDictionary())
+    cameraPolicy = geomPolicy
+
+
+
+    if self.policy is None:
+            self.policy = pexPolicy.Policy()
+    self.policy.mergeDefaults(defPolicy)
+    cameraPolicy = clipboard.get(self.policy.getString("inputKeys.cameraPolicy"))
+    camera = cameraGeomUtils.makeCamera(cameraPolicy)
+
     root = PropertySet()
-  
-    filename = os.path.join(eups.productDir("afwdata"), "CFHT", "D4", "cal-53535-i-797722_1_img.fits")
-    root.set("inputPathName", filename)
-    # should create: /lsst/DC3/stacks/gcc433/04jun/Linux64/afwdata/svn1397/CFHT/D4/cal-53535-i-797722_1_img.fits
-    print "inputPathname: %s" % (filename)
+    root.set("cameraInfo", camera)
 
     externalEventTransmitter.publish(root)
 

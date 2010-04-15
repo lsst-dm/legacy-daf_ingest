ccc3027774673cbf55b950941e8e99e934098485
rallsman <rallsman@git.lsstcorp.org>
2010-04-15 18:45:02 +0000

    Intermediate save of change for JobOffice/Butler use.

--- pipeline/ISR/harness/triggerISREventTransmitter.py
+++ pipeline/ISR/harness/triggerISREventTransmitter.py
@@ -21,5 +21,9 @@ if __name__ == "__main__":
     externalEventTransmitter = events.EventTransmitter(eventBrokerHost, "triggerISREvent")
     root = PropertySet()
 
+    # parameters provided on the event trigger. 
+    #     FWHM seems a curious trigger event.
+    #root.set("fwhm", 5.)
+
     externalEventTransmitter.publish(root)
 

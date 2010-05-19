633d500629c80eee6b5f6c3115f22ad99b6e0893
rallsman <rallsman@git.lsstcorp.org>
2010-05-19 02:50:22 +0000

    Remove use of CCD 3 for CFHT datasets. Start update for orca use.

--- pipeline/joboffice.sh
+++ pipeline/joboffice.sh
@@ -10,8 +10,12 @@ if [ $VERBOSITY = "None" ]; then
      VERBOSITY="silent"
 fi
 
-(joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/CcdAssembly/cfht-ca-joboffice.paf >/dev/null 2>&1 &) &
 (joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/ISR/cfht-isr-joboffice.paf >/dev/null 2>&1 &) &
+
+(joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/CcdAssembly/cfht-ca-joboffice.paf >/dev/null 2>&1 &) &
+(joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/CrSplit/cfht-crSplit-joboffice.paf >/dev/null 2>&1 &) &
+(joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/ImgChar/cfht-imgChar-joboffice.paf >/dev/null 2>&1 &) &
+(joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $DC3ROOT/$RUNID/work $DATAREL_DIR/pipeline/SFM/cfht-sfm-joboffice.paf >/dev/null 2>&1 &) &
 sleep 2
 echo done starting job offices
 exit 0

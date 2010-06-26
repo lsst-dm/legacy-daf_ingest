4713c0e6abf31d8d6e16ff9a89191941324da2a6
rallsman <rallsman@git.lsstcorp.org>
2010-06-26 03:14:09 +0000

    Abe Integration of ISR refinements (#1280)

--- bin/runOrca/cfht-joboffice-abe.sh
+++ bin/runOrca/cfht-joboffice-abe.sh
@@ -10,15 +10,15 @@ if [ $VERBOSITY = "None" ]; then
      VERBOSITY="silent"
 fi
 
-joboffice.py -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ISR/cfht-isr-joboffice.paf > /dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ISR/cfht-isr-joboffice.paf > /dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $defaultRoot/$RUNID/work $DATAREL_DIR/pipeline/CcdAssembly/cfht-ca-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CcdAssembly/cfht-ca-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $defaultRoot/$RUNID/work $DATAREL_DIR/pipeline/CrSplit/cfht-crSplit-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CrSplit/cfht-crSplit-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $defaultRoot/$RUNID/work $DATAREL_DIR/pipeline/ImgChar/cfht-imgChar-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ImgChar/cfht-imgChar-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $defaultRoot/$RUNID/work $DATAREL_DIR/pipeline/SFM/cfht-sfm-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/SFM/cfht-sfm-joboffice.paf >/dev/null 2>&1 &
 
 echo done starting job offices
 wait
--- bin/runOrca/cfht-setupForOrcaUse-abe.sh
+++ bin/runOrca/cfht-setupForOrcaUse-abe.sh
@@ -5,7 +5,7 @@
 export LSST_DEVEL=/u/ac/rallsman/lsstSandbox
 source /cfs/projects/lsst/DC3/stacks/default/loadLSST.sh
 setup datarel
-setup -j afw 3.6.0-svn15913
+setup -j afw 3.6.0-svn15969
 setup -j ap 3.2.1+svn15516
 setup -j ip_isr 3.4.5+3
 setup -j ip_utils 3.0.0+1
@@ -15,6 +15,7 @@ setup -j meas_astrom 3.2.0+svn15916
 setup -j meas_utils 3.2.0+svn15840
 setup -j meas_pipeline 3.2.0+svn15431
 setup -j daf_persistence 3.3.13+svn15478
+setup -j pex_harness 3.6.9+svn15978
 setup -j ctrl_provenance 3.1
 setup -j obs_cfht 3.0.7+svn15480
 setup astrometry_net_data cfhtlsDeep

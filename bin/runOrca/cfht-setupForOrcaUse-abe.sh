8d65eec0861199d15b4d9f4223c38b1432a7b036
rallsman <rallsman@git.lsstcorp.org>
2010-06-28 00:54:36 +0000

    Preparing to process single Imsim CCD on ABE to verify file setup (#1280)

--- bin/runOrca/cfht-setupForOrcaUse-abe.sh
+++ bin/runOrca/cfht-setupForOrcaUse-abe.sh
@@ -5,18 +5,21 @@
 export LSST_DEVEL=/u/ac/rallsman/lsstSandbox
 source /cfs/projects/lsst/DC3/stacks/default/loadLSST.sh
 setup datarel
-setup -j afw 3.6.0-svn15969
+setup -j afw afw_3.6.0
 setup -j ap 3.2.1+svn15516
 setup -j ip_isr 3.4.5+3
 setup -j ip_utils 3.0.0+1
 setup -j ip_pipeline  3.2.0+svn15431
-setup -j meas_algorithms 3.2.0+2
-setup -j meas_astrom 3.2.0+svn15916
-setup -j meas_utils 3.2.0+svn15840
-setup -j meas_pipeline 3.2.0+svn15431
-setup -j daf_persistence 3.3.13+svn15478
-setup -j pex_harness 3.6.9+svn15978
+setup -j meas_algorithms meas_algorithms_3.2.0+2
+setup -j meas_astrom meas_astrom_3.2.0+svn15995
+setup -j meas_utils meas_utils_3.2.1
+setup -j meas_pipeline meas_pipeline_3.2.0+svn15995
+setup -j daf_persistence daf_persistence_3.3.14
+setup -j pex_harness pex_harness_3.6.10
+setup -j pex_policy pex_policy_3.5.2
 setup -j ctrl_provenance 3.1
+# Only for CFHTLS run
 setup -j obs_cfht 3.0.7+svn15480
 setup astrometry_net_data cfhtlsDeep
+#
 eups list --setup
--- bin/runOrca/imsim-setupForOrcaUse-abe.sh
+++ bin/runOrca/imsim-setupForOrcaUse-abe.sh
@@ -5,16 +5,21 @@
 export LSST_DEVEL=/u/ac/rallsman/lsstSandbox
 source /cfs/projects/lsst/DC3/stacks/default/loadLSST.sh
 setup datarel
-setup -j afw 3.6.0-svn15913
+setup -j afw afw_3.6.0
 setup -j ap 3.2.1+svn15516
 setup -j ip_isr 3.4.5+3
 setup -j ip_utils 3.0.0+1
 setup -j ip_pipeline  3.2.0+svn15431
-setup -j meas_algorithms 3.2.0+2
-setup -j meas_astrom 3.2.0+svn15916
-setup -j meas_utils 3.2.0+svn15840
-setup -j meas_pipeline 3.2.0+svn15431
-setup -j daf_persistence 3.3.13+svn15478
+setup -j meas_algorithms meas_algorithms_3.2.0+2
+setup -j meas_astrom meas_astrom_3.2.0+svn15995
+setup -j meas_utils meas_utils_3.2.1
+setup -j meas_pipeline meas_pipeline_3.2.0+svn15995
+setup -j daf_persistence daf_persistence_3.3.14
+setup -j pex_harness pex_harness_3.6.10
+setup -j pex_policy pex_policy_3.5.2
+setup -j ctrl_provenance 3.1
+# only for ImSim run
 setup -j obs_lsstSim 3.0.10+svn15481
 setup astrometry_net_data imsim_20100611_robyn
+#
 eups list --setup

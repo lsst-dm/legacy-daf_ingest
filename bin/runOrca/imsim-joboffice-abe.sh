e259516b8b53eb8b4db9f3b6afa00233bd1999e6
rallsman <rallsman@git.lsstcorp.org>
2010-06-29 19:01:07 +0000

    Update to include procsesing of all PT1 pipelines (#1280)

--- bin/runOrca/imsim-joboffice-abe.sh
+++ bin/runOrca/imsim-joboffice-abe.sh
@@ -12,13 +12,13 @@ fi
 
 joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ISR/imsim-isr-joboffice.paf > /dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CcdAssembly/imsim-ca-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CcdAssembly/imsim-ca-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CrSplit/imsim-crSplit-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/CrSplit/imsim-crSplit-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ImgChar/imsim-imgChar-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/ImgChar/imsim-imgChar-joboffice.paf >/dev/null 2>&1 &
 
-#joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/SFM/imsim-sfm-joboffice.paf >/dev/null 2>&1 &
+joboffice.py  -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME $DATAREL_DIR/pipeline/SFM/imsim-sfm-joboffice.paf >/dev/null 2>&1 &
 
 echo done starting job offices
 wait

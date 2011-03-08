59e5fed086365da6bb3fc06673102b4c8d5b3b72
srp <srp@git.lsstcorp.org>
2011-03-08 22:20:52 +0000

    Changes for ticket 1594

--- pipeline/PT1Pipe/joboffice-ImSim.sh
+++ pipeline/PT1Pipe/joboffice-ImSim.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # These arguments are in the same location for launchPipeline.sh and are
 # reused here
@@ -10,7 +10,8 @@ if [ $VERBOSITY = "None" ]; then
      VERBOSITY="silent"
 fi
 
-(nohup joboffice.py -D -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME/.. $DATAREL_DIR/pipeline/PT1Pipe/jobOffice-ImSim.paf >/dev/null 2>&1 &) &
-sleep 2
-echo done starting job office
-exit 0
+joboffice.py -L $VERBOSITY -r $RUNID -b lsst8.ncsa.uiuc.edu -d $LOGDIRNAME/.. $DATAREL_DIR/pipeline/PT1Pipe/jobOffice-ImSim.paf >/dev/null 2>&1 &
+pid=$!
+echo done. Now starting job office
+wait $pid
+exit $?

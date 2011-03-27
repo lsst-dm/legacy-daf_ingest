c6f5c9654daccda2c8630bec4310b173fd3d8130
daues <daues@git.lsstcorp.org>
2011-03-27 19:24:57 +0000

    edit lsst_devel, typo

--- pipeline/Weekly/load_env_trunk.sh
+++ pipeline/Weekly/load_env_trunk.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 export SHELL=/bin/bash
-# export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox 
-export LSST_DEVEL=/home/buildbot/buildbotSandbox 
-source /lsst/DC3/stacks/default/loadLSST.sh 
 
+export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox 
+# export LSST_DEVEL=/home/buildbot/buildbotSandbox 
+
+source /lsst/DC3/stacks/default/loadLSST.sh 
 
 setup datarel  
 
--- pipeline/Weekly/stack_trunk.sh
+++ pipeline/Weekly/stack_trunk.sh
@@ -4,12 +4,11 @@ export SHELL=/bin/bash
 
 CURRENT=$PWD
 
-# export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox
-export LSST_DEVEL=/home/buildbot/buildbotSandbox
+export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox
+# export LSST_DEVEL=/home/buildbot/buildbotSandbox
 source /lsst/DC3/stacks/default/loadLSST.sh
 
-seup datarel 
+setup datarel 
 
 cd $CURRENT 
-
 

5914d155165300b3cac0bec96496dd4a58d126c6
rallsman <rallsman@git.lsstcorp.org>
2011-03-30 16:22:50 +0000

    Add new '-debug' option to bootstrap procedure to setup for truncated raft use.

--- pipeline/Weekly/bootstrap.sh
+++ pipeline/Weekly/bootstrap.sh
@@ -1,18 +1,63 @@
-#!/bin/sh
+#!/bin/bash
+#
+# LSST Data Management System
+# Copyright 2008, 2009, 2010, 2011 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
 
-if [ $# -eq 0 ]
-then
-echo "$0 : You must supply 'trunk' or 'tags' as a command line argument"
-exit 1
+#--------------------------------------------------------------------------
+usage() {
+#80 cols  ................................................................................
+    echo ""
+    echo "Usage: $0 [-debug] <branch>"
+    echo "Bootstrap and then process a Weekly Production Run."
+    echo
+    echo "Parameters (must be in this order):"
+    echo "      -debug: use limited raft set for debug run."
+    echo "    <branch>: one of"
+    echo "              'tags'  - use stack comprised of current tags;"
+    echo "              'trunk' - use stack comprised of trunk versions."
+    echo ""
+}
+#--------------------------------------------------------------------------
+
+
+PROG=$0
+
+if [ $# -eq 0 ] ; then
+    usage
+    exit 1
 fi
 
-
-if [ $1 = 'tags' -o $1 = 'trunk' ]
-then
-echo "Running for stack: $1 "
+if [ "$1" = "-debug" ] ; then
+    DEBUG_DATA=0
+    echo "$PROG : Using the debug miminal-raft setup."
+    shift 1
 else
-echo "$1 : You must supply 'trunk' or 'tags' as a command line argument"
-exit 1
+    DEBUG_DATA=1
+fi
+
+if [ "$1" = "tags" -o "$1" = "trunk" ] ; then
+    echo "$PROG: Running for stack: $1 "
+else
+    usage
+    exit 1
 fi
 
 
@@ -22,20 +67,19 @@ i=`date "+%Y_%m%d"`
 # stackType="trunk" stackType="tags"
 stackType=$1
 
-if [ $stackType = 'tags' ]
-then
+if [ "$stackType" = "tags" ] ; then
 
-export SHELL=/bin/bash
-export LSST_HOME=/lsst/DC3/stacks/default
-source /lsst/DC3/stacks/default/loadLSST.sh
-setup datarel
+    export SHELL=/bin/bash
+    export LSST_HOME=/lsst/DC3/stacks/default
+    source /lsst/DC3/stacks/default/loadLSST.sh
+    setup datarel
 
 else
 
-export SHELL=/bin/bash
-export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox
-source /lsst/DC3/stacks/default/loadLSST.sh
-setup datarel
+    export SHELL=/bin/bash
+    export LSST_DEVEL=/lsst/home/buildbot/buildbotSandbox
+    source /lsst/DC3/stacks/default/loadLSST.sh
+    setup datarel
 
 fi
 
@@ -54,16 +98,13 @@ cd pipeline
 echo "copy weekly production files"
 cp Weekly/* .
 
+if [ $DEBUG_DATA = 0 ]; then
+    cp  Weekly/debug_weekly.input weekly.input
+fi
+
 echo "copy PT1Pipe policy"
 cp PT1Pipe/main-ImSim.paf . 
 
 echo "launch weekly production"
 nohup ./run_weekly_production.sh ${stackType}  >& weekly_production_$i.log
-
-
-
-
-
-
-
 

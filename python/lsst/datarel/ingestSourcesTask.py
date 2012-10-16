870d81f94f92e723f9ffb35dabddffe8de4cc427
Kian-Tat Lim <ktl@slac.stanford.edu>
2012-10-16 08:41:12 -0700

    Add some doc strings, make certain arguments required.

--- python/lsst/datarel/ingestSourcesTask.py
+++ python/lsst/datarel/ingestSourcesTask.py
@@ -9,31 +9,57 @@ import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 
 class ColumnFormatter(object):
+    """A class to format a column in an afw.SourceCatalog.
+
+    A little tricky because a column's values may be composite entities
+    (coordinates, matrixes, etc.).
+
+    This class is basically a container for a SQL type, a function returning
+    SQL column names, and a function returning the formatted value of a
+    column."""
+
     def __init__(self, sqlType, columnNameCallable, formatValueCallable):
+        """Store the column formatting information."""
         self.sqlType = sqlType
         self.columnNameCallable = columnNameCallable
         self.formatValueCallable = formatValueCallable
 
     def getSqlType(self):
+        """Return the SQL type (e.g. BIGINT, DOUBLE) for the column's basic
+        values."""
         return self.sqlType
 
     def getColumnNames(self, baseName):
+        """Return an iterable of the names that should be used for columns in
+        SQL given a SQL-compatible base name derived from the catalog's column
+        name."""
         return self.columnNameCallable(baseName)
 
     def formatValue(self, value):
+        """Return a string suitable for inclusion in an INSERT/REPLACE
+        statement (not a CSV file) resulting from formatting the column's
+        value.  One value should be provided for each of the column names that
+        had been returned, of course.  Values should be separated by commas.
+        This method also handles changing "None" values to SQL NULLs."""
         if value is None:
             return "NULL"
         return self.formatValueCallable(value)
 
         
 def _formatNumber(fmt, number):
+    """Auxiliary function for formatting a number, handling conversion of NaN
+    and infinities to NULL."""
     if math.isnan(number) or math.isinf(number):
         return "NULL"
     return fmt % (number,)
 
 def _formatList(fmt, list):
+    """Auxiliary function for formatting a list of numbers using a common
+    format, joining the results with commas."""
     return ", ".join([_formatNumber(fmt, x) for x in list])
 
+"""Describe how to handle each of the column types. Array and Cov (plain)
+types are not yet processed."""
 columnFormatters = dict(
         Flag = ColumnFormatter("BIT", lambda x: (x,),
             lambda v: "1" if v else "0"),
@@ -83,6 +109,7 @@ columnFormatters = dict(
         )
 
 class IngestSourcesConfig(pexConfig.Config):
+    """Configuration for the IngestSourcesTask."""
     allowReplace = pexConfig.Field(
             "Allow replacement of existing rows with the same source IDs",
             bool, default=False)
@@ -91,28 +118,41 @@ class IngestSourcesConfig(pexConfig.Config):
             str, default="id")
 
 class IngestSourcesTask(pipeBase.CmdLineTask):
+    """Task to ingest a SourceCatalog of arbitrary schema into a database
+    table."""
+
     ConfigClass = IngestSourcesConfig
     _DefaultName = "ingestSources"
 
     @classmethod
     def _makeArgumentParser(cls):
+        """Extend the default argument parser with database-specific
+        arguments and the dataset type for the Sources to be read.""" 
         parser = pipeBase.ArgumentParser(name=cls._DefaultName)
-        parser.add_argument("-H", "--host", dest="host",
+        parser.add_argument("-H", "--host", dest="host", required=True,
                 help="Database hostname")
-        parser.add_argument("-D", "--database", dest="db",
+        parser.add_argument("-D", "--database", dest="db", required=True,
                 help="Database name")
         parser.add_argument("-U", "--user", dest="user",
                 help="Database username (optional)", default=None)
         parser.add_argument("-P", "--port", dest="port",
                 help="Database port number (optional)", default=3306)
-        parser.add_argument("-t", "--table", dest="tableName",
+        parser.add_argument("-t", "--table", dest="tableName", required=True,
                 help="Table to ingest into")
         parser.add_argument("-d", "--dataset-type", dest="datasetType",
+                required=True,
                 help="Dataset type of Sources to ingest")
         return parser
 
     @classmethod
     def runParsedCmd(cls, parsedCmd):
+        """Override the default method for running the parsed command.
+        Necessary because the task needs to be instantiated with more than
+        just the data id and because we want to connect to the database only
+        once for all data ids specified.  Prevents the use of
+        multiprocessing.  Note that the config and metadata are written using
+        the first data id given, not each of them."""
+
         task = cls(tableName=parsedCmd.tableName,
                 datasetType=parsedCmd.datasetType,
                 host=parsedCmd.host, db=parsedCmd.db,
